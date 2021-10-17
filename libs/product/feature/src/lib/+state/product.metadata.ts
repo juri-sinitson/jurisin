3f022166b5cf6803dd7db74c095684834545aabb
Juri Sinitson <juri.sinitson@omeany.de>
2021-10-18 00:30:02 +0200

    refactor(uneeded): removing
    * Using the command
    nx g remove <library-name or app-name>
    * See also https://nx.dev/l/a/structure/grouping-libraries

--- libs/product/feature/src/lib/+state/product.metadata.ts
+++
@@ -1,6 +0,0 @@
-import { EntityMetadata } from '@ngrx/data';
-import { Product } from '../types/product';
-
-export const productEntityMetadata: EntityMetadata<Product> = {
-  entityName: 'Product',
-};
