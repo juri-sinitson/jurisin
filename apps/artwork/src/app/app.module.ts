48c35a637b59a268ce7c914a20441dc2f64970ed
Juri Sinitson <juri.sinitson@omeany.de>
2021-10-17 19:39:09 +0200

    refactor(tools): install @ngrx/store-devtools

--- apps/artwork/src/app/app.module.ts
+++ apps/artwork/src/app/app.module.ts
@@ -1,15 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { environment } from '../environments/environment';
+
 import { AppComponent } from './app.component';
 
 import { GridViewModule } from '@jurisin/artwork/feature';
+import { StoreModule } from '@ngrx/store';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [
     BrowserModule,
     GridViewModule,
+
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge: 25, // Retains last 25 states
+      logOnly: environment.production, // Restrict extension to log-only mode
+      autoPause: true, // Pauses recording actions and state changes when the extension window is not open
+    }),
   ],
   providers: [],
   bootstrap: [AppComponent],
