0342362b23021c3423b14c17c2456cc764e5ca33
Juri Sinitson <juri.sinitson@omeany.de>
2022-03-25 16:06:21 +0100

    fixup! making test green and adding the code coverage support

---
+++ apps/artwork-e2e/src/plugins/index.ts
@@ -0,0 +1,10 @@
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+module.exports = (on: any, config: any) => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  require('@cypress/code-coverage/task')(on, config)
+  // include any other plugin code...
+
+  // It's IMPORTANT to return the config object
+  // with any changed environment variables
+  return config
+};
