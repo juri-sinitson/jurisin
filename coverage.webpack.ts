bfc5512c46dc98ecab67c76acd9ffedbfbe5141a
Juri Sinitson <juri.sinitson@omeany.de>
2022-03-28 11:17:46 +0200

    fixup! it works but no coverage comes

---
+++ coverage.webpack.ts
@@ -0,0 +1,23 @@
+import * as path from 'path';
+
+export default {
+  module: {
+    rules: [
+      {
+        test: /\.(js|ts)$/,
+        loader: '@jsdevtools/coverage-istanbul-loader',
+        options: { esModules: true },
+        enforce: 'post',
+        include: [
+          path.join(__dirname, '.', 'libs'),
+          path.join(__dirname, '.', 'apps'),
+        ],
+        exclude: [
+          /\.(e2e|spec)\.ts$/,
+          /node_modules/,
+          /(ngfactory|ngstyle)\.js/,
+        ],
+      },
+    ],
+  },
+};
