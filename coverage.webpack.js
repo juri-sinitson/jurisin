99945c0185d544f2711691d72bff45f4a4b127d8
Juri Sinitson <juri.sinitson@omeany.de>
2022-04-11 10:29:19 +0200

    fixup! improvement

--- coverage.webpack.js
+++ coverage.webpack.js
@@ -4,14 +4,10 @@ module.exports = {
   module: {
     rules: [
       {
-        test: /\.(js|ts)$/,
+        test: /\.(js|ts|html)$/,
         loader: '@jsdevtools/coverage-istanbul-loader',
         options: { esModules: true },
         enforce: 'post',
-        include: [
-          path.join(__dirname, '/', 'libs'),
-          path.join(__dirname, '/', 'apps'),
-        ],
         exclude: [
           /\.(e2e|spec)\.ts$/,
           /node_modules/,
